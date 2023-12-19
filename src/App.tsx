f6bcd6f9b38d872e02093dabf5271b82b38e59df
dibrugh <dibrugh@gmail.com>
2023-12-19 16:29:45 +0300

    ops: connect react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* import "bootstrap/dist/css/bootstrap.min.css"; */
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+	return (
+	<Routes>
+		<Route path="/" element={<h1>Hi</h1>} />
+		<Route path="/new" element={<h1>Home</h1>} />
+    <Route path="/:id">
+      <Route index element={<h1>Show</h1>}/>
+      <Route path="edit" element={<h1>Edit</h1>}/>
+    </Route>
+		{/* matches everything, redirects to the set page (i.e home) */}
+		<Route path="*" element={<Navigate to={"/"} />}></Route>
+	</Routes>
   )
 }
-
-export default App
+export default App;
